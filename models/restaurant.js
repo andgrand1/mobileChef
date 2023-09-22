f07cd1d90f7356549bcd947662218dde0fef1e6a
Abigail Peloquin <abigail.turnwall@gmail.com>
2023-09-21 19:29:07 -0500

    added restaurant stuff

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,22 +1,50 @@
 const Sequelize = require('sequelize');
 const { sequelize } = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
 
-const Restaurant = sequelize.define('Restaurant', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.TEXT,
-  },
-  location: {
-    type: Sequelize.STRING,
-  },
-  // Add more properties as needed, such as opening hours, contact information, etc? 
-});
+// const Restaurant = sequelize.define('Restaurant', {
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: Sequelize.TEXT,
+//   },
+//   location: {
+//     type: Sequelize.STRING,
+//   },
+//   // Add more properties as needed, such as opening hours, contact information, etc? 
+// });
 
 const MenuItem = require('./menuItem');
 Restaurant.hasMany(MenuItem);
 
+Restaurant.init(
+  {
+    restaurant_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    restaurant_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    restaurant_description: {
+      type: DataTypes.TEXT,
+    },
+    restaurant_location: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "restaurant",
+  }
+);
 
 module.exports = Restaurant;

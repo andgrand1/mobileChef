ee100c7c70c4876925be28e745350fae7507fcc6
Andy Granda <andygranda@Andys-MacBook-Pro.local>
2023-09-21 17:33:37 -0700

    added validation to menuItem.js

--- models/menuItem.js
+++ models/menuItem.js
@@ -5,6 +5,9 @@ const MenuItem = sequelize.define('MenuItem', {
   name: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: true, // Ensure the name is not empty
+    },
   },
   description: {
     type: Sequelize.TEXT,
@@ -12,6 +15,9 @@ const MenuItem = sequelize.define('MenuItem', {
   price: {
     type: Sequelize.DECIMAL(10, 2), // Example: 9.99
     allowNull: false,
+    validate: {
+      min: 0, // Ensure the price is non-negative
+    },
   },
   // Add more properties as needed
 });

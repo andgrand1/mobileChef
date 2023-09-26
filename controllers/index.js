e221e49fc7643f81b863f2f4120d809ccea0312c
Shawn Kim <kimshawnj@gmail.com>
2023-09-25 19:49:40 -0500

    nothing

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,5 @@ const apiRoutes = require("./api");
 const Routes = require("../routes");
 router.use("/api", apiRoutes);
 router.use("/routes", Routes);
-router.get("/", (req, res) => {
-  res.render("home", { title: "Food Delivery App" });
-});
 
 module.exports = router;

33ce6df239959eb05e7c6b6337971e65729c5536
Annie <anniebui0110@gmil.com>
2023-09-25 19:35:46 -0500

    cart handlebar & js

---
+++ public/js/cart.js
@@ -0,0 +1,20 @@
+// used cleaves js code
+
+// credit card
+new Cleave('.my-input', {
+    creditCard: true,
+    onCreditCardTypeChanged: function (type) {
+    }
+    // update UI ...
+});
+
+// phone
+var cleavePhone = new Cleave('.input-phone', {
+    phone: true,
+    phoneRegionCode: '{country}'
+});
+
+// date
+var cleaveDate = new Cleave('.input-date', {
+    date: true
+});

b120af660ef870bb2e2aeb6402716c0f3ec80ca8
Annie <anniebui0110@gmil.com>
2023-09-27 19:02:07 -0500

    js button

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+// Mcdonald.handlebars
+var cardLink = document.querySelector(".card-link");
+
+//cart
+var paymentBtn = document.querySelector("#paymentbtn");
+
+//find.handlebars
+var mcdonaldBtn = document.querySelector("#mcdonaldBtn");
+
+// login.handlebars
+var loginBtn = document.querySelector("#loginBtn");
+var signupButton = document.querySelector("#signupButton");
+
+
+// Add event listener to generate button
+cardLink.addEventListener("click");
+paymentBtn.addEventListener("click");
+mcdonaldBtn = addEventListener("click");
+signupButton = addEventListener("click");
+loginBtn = addEventListener("click");
+
+
